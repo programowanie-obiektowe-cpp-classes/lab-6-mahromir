f2e356cc835db859b4a96e37620ec6efdc347ec1
Marek Polewski <marek.polewski.stud@pw.edu.pl>
2025-11-28 10:43:44 +0100

    Lab6

--- foo.hpp
+++ foo.hpp
@@ -8,9 +8,12 @@
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    std::vector< char > retval(people.size());
-
-    // Twoja implementacja tutaj
-
+    std::vector< char > retval;
+    auto func=[&](Human& it){
+        it.birthday();
+        retval.insert(retval.begin(),it.isMonster() ? 'n' : 'y');
+    };
+    std::for_each(people.begin(), people.end(), func);
+   
     return retval;
 }
